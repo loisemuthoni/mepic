f4d3856037ebf456bdb765513b93bca7cee4402c
Moringa School <loisemburu01@gmail.com>
2020-02-02 21:55:54 +0300

    created a category class

--- photo/models.py
+++ photo/models.py
@@ -10,3 +10,12 @@ class Location(models.Model):
 
     def save_location(self):
         self.save()
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()        

89e75e1323e93ccc3e1ab9ad09e9c8b63ea85815
Moringa School <loisemburu01@gmail.com>
2020-02-02 21:59:05 +0300

    added category, location and images to the admin

--- photo/admin.py
+++ photo/admin.py
@@ -2,3 +2,12 @@ from django.contrib import admin
 from .models import Images, Category, Location
 
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "pub_date", "location", "category"]
+    class Meta:
+        model = Images
+
+admin.site.register(Images, ImageAdmin )
+admin.site.register(Category)
+admin.site.register(Location)

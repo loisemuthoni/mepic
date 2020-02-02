7239e52729269637d96069262612bb349d1ea59e
Moringa School <loisemburu01@gmail.com>
2020-02-02 22:04:44 +0300

    created a view fuction for views

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import rende
+from django.http import HttpResponse, Http404
+from django.templatetags.static import static
+from .models import Images, Location, Category
+from django.db.models.query import QuerySet
 
 # Create your views here.
+def home(request):
+    images = Images.objects.all()
+    location = Location.objects.all()
+    context = {
+        "images":images,
+        "location":location,
+    }
+    return render(request, 'home.html', context)

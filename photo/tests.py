3c203e629715efedcb3b6a3a427a5a102672a1ab
Moringa School <loisemburu01@gmail.com>
2020-02-03 08:59:09 +0300

    created a test function for the location

--- photo/tests.py
+++ photo/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Images, Location,Category
 
 # Create your tests here.
+
+class LocationTest(TestCase):
+    def setUp(self):
+        self.location = Location(name = 'Nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save(self):
+        self.location.save_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) > 0)

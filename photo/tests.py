16991e7b453037a52b5659d3364380de0fbba3b3
Moringa School <loisemburu01@gmail.com>
2020-02-03 09:00:58 +0300

    added a testcase for image

--- photo/tests.py
+++ photo/tests.py
@@ -14,3 +14,26 @@ class LocationTest(TestCase):
         self.location.save_location()
         location = Location.objects.all()
         self.assertTrue(len(location) > 0)
+
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.category = Category(name = 'Food')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save(self):
+        self.category.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+class ImageTest(TestCase):
+
+    def setUp(self):
+        self.location = Location(name= 'Nairobi')
+        self.location.save()
+        self.category = Category(name = 'name')
+        self.category.save()
+        self.image = Images(image_path = 'irene.jpg', name = 'irene', description = 'The best food eating game', location = self.location, category = self.category)
+
+

fe5b5eef598f7c9678853a4910feee204bf5aa5b
Moringa School <loisemburu01@gmail.com>
2020-02-03 22:40:51 +0300

    changes on the urls

--- gallery/settings.py
+++ gallery/settings.py
@@ -61,15 +61,6 @@ STATICFILES_DIRS = (
 )
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4$4^p5$zu()u9*xtwts93thj2bekg6vxl*y6-muh+-nvcov69*'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-
-# ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -151,16 +142,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
